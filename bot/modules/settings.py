cbb8d13ff3fd5ba82fdcd9a114cd487a3112f136
MxVm-source <judder.page@yahoo.com>
2025-11-19 03:47:04 +0100

    Delete bot/modules/settings.py

--- bot/modules/settings.py
+++
@@ -1,7 +0,0 @@
-# settings.py
-import os
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CRYPTOWATCH_CHAT_ID = os.getenv("CRYPTOWATCH_CHAT_ID")
-TIMEZONE = os.getenv("TIMEZONE", "Europe/Brussels")
-CRYPTOWATCH_ENABLED = os.getenv("CRYPTOWATCH_ENABLED", "true").lower() == "true"
