ba86b6dad32424c085798696331ae4965056f2e5
MxVm-source <judder.page@yahoo.com>
2025-11-25 09:46:07 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,8 @@
 import os
 import threading
-import time
-from bot.modules.datafeed_bitget import start_bitget_watcher
+import timefrom bot.modules.datafeed_bitget import start_bitget_watcher  # <-- line 4
 
 from apscheduler.schedulers.background import BackgroundScheduler
-
 from bot.utils import send_text, get_updates
 from bot.modules import trumpwatch, fedwatch, cryptowatch, cryptowatch_daily
 # trumpwatch_live imported in __main__
