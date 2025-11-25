fab83975e10de469d89fff7667c7147d0da2c91d
MxVm-source <judder.page@yahoo.com>
2025-11-25 16:24:06 +0100

    Update fedwatch.py

--- bot/modules/fedwatch.py
+++ bot/modules/fedwatch.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 from zoneinfo import ZoneInfo
 
 from bot.utils import send_text
-from bot.datafeed_bitget import get_ticker
+
 
 # ---- HTML Source (FOMC Calendar) ----
 FED_HTML_URL = "https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm"
